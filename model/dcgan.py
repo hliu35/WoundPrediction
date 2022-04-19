27bc00cb5ee93d78866fd677a4731142349203b4
hliu35 <hliu35@ucsc.edu>
2022-04-19 11:03:09 -0700

    added dcgan.py

---
+++ model/dcgan.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5, 1, 2)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 5, 1, 2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.fc1  = nn.Linear(64*28*28+1000, 1024)
+        self.fc2 = nn.Linear(1024, 1)
+        self.fc3 = nn.Linear(10, 1000)
+
+    def forward(self, x, labels):
+        batch_size = x.size(0)
+        x = x.view(batch_size, 1, 28,28)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = x.view(batch_size, 64*28*28)
+        y_ = self.fc3(labels)
+        y_ = F.relu(y_)
+        x = torch.cat([x, y_], 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return F.sigmoid(x)
+
+class Generator(nn.Module):
+    def __init__(self, z_dim):
+        self.z_dim = z_dim
+        super(Generator, self).__init__()
+        self.fc2 = nn.Linear(10, 1000)
+        self.fc = nn.Linear(self.z_dim+1000, 64*28*28)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.deconv1 = nn.ConvTranspose2d(64, 32, 5, 1, 2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.deconv2 = nn.ConvTranspose2d(32, 1, 5, 1, 2)
+
+    def forward(self, x, labels):
+        batch_size = x.size(0)
+        y_ = self.fc2(labels)
+        y_ = F.relu(y_)
+        x = torch.cat([x, y_], 1)
+        x = self.fc(x)
+        x = x.view(batch_size, 64, 28, 28)
+        x = self.bn1(x) 
+        x = F.relu(x)
+        x = self.deconv1(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = self.deconv2(x)
+        x = F.sigmoid(x)
+        return x
+
+
+
