ceaa2aa2103d5b9e2fb1cc73a6f596d3fa46c394
hliu35 <hliu35@ucsc.edu>
2022-04-19 11:55:12 -0700

    added label in getitem

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -2,6 +2,9 @@ import os
 
 from PIL import Image
 
+import numpy as np
+import pandas as pd
+
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
@@ -11,8 +14,8 @@ from torchvision.io import read_image
 
 
 class WoundImageDataset(Dataset):
-    def __init__(self, img_list, transform=None, target_transform=None):
-        #self.img_labels = pd.read_csv(annotations_file)
+    def __init__(self, img_list, annotations_file="../data/labels.csv", transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
         self.img_list = img_list
         self.transform = transform
         self.target_transform = target_transform
@@ -23,6 +26,9 @@ class WoundImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.img_list[idx]
+        filename = img_path.split("/")[-1]
+
+        label_row = self.img_labels.loc[self.img_labels["Image"]==filename]
         #image = read_image(img_path) # using pytorch read image yields error in transform ToTensor()
         image = Image.open(img_path)
         #label = self.img_labels.iloc[idx, 1]
@@ -31,5 +37,6 @@ class WoundImageDataset(Dataset):
         #if self.target_transform:
         #    label = self.target_transform(label)
         image = image[:3, :]
-        
-        return image
+        label = torch.as_tensor(label_row.iloc[0, 1:]).T
+
+        return image, label
