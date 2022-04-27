930b41978396353cbfc982b567b685b64fc561e8
hliu35 <hliu35@ucsc.edu>
2022-04-27 12:45:45 -0700

    introduces label thresholding with two custom thresholds

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -12,6 +12,9 @@ from torchvision.transforms import ToTensor, Resize
 
 from torchvision.io import read_image
 
+THRESHOLD_1 = 0.75
+THRESHOLD_2 = 0.4
+
 
 class WoundImageDataset(Dataset):
     def __init__(self, img_list, annotations_file="../data/labels.csv", transform=None, target_transform=None):
@@ -37,6 +40,23 @@ class WoundImageDataset(Dataset):
         #if self.target_transform:
         #    label = self.target_transform(label)
         image = image[:3, :]
-        label = torch.FloatTensor(label_row.iloc[0, 1:]).T
+        label = label_row.iloc[0, 1:]
+
+        label = self.thresholding(label,)    
+        label = torch.FloatTensor(label).T
+        
 
         return image, label
+    
+
+    def thresholding(self, label, thresh_1=THRESHOLD_1, thresh_2=THRESHOLD_2):
+
+        if np.count_nonzero(label >= thresh_1) == 1:
+            new_label = np.where(label >= thresh_1, 1, 0)
+            return new_label
+
+        elif np.count_nonzero(label >= thresh_2) == 2:
+            new_label = np.where(label >= thresh_2, 0.5, 0)
+            return new_label
+        
+        return label
