19e25683b02003c29b9b9b04091eac4933e9a517
hliu35 <hliu35@ucsc.edu>
2022-04-28 22:28:47 -0700

    moved thresholding script to augment.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -12,9 +12,10 @@ from torchvision.transforms import ToTensor, Resize
 
 from torchvision.io import read_image
 
+import augment as AUG
+
 THRESHOLD_1 = 0.75
 THRESHOLD_2 = 0.4
-
 
 class WoundImageDataset(Dataset):
     def __init__(self, img_list, annotations_file="../data/labels.csv", transform=None, target_transform=None):
@@ -42,21 +43,9 @@ class WoundImageDataset(Dataset):
         image = image[:3, :]
         label = label_row.iloc[0, 1:]
 
-        label = self.thresholding(label,)    
+        label = AUG.thresholding(label,thresh_1=THRESHOLD_1, thresh_2=THRESHOLD_2)    
         label = torch.FloatTensor(label).T
         
 
         return image, label
     
-
-    def thresholding(self, label, thresh_1=THRESHOLD_1, thresh_2=THRESHOLD_2):
-
-        if np.count_nonzero(label >= thresh_1) == 1:
-            new_label = np.where(label >= thresh_1, 1, 0)
-            return new_label
-
-        elif np.count_nonzero(label >= thresh_2) == 2:
-            new_label = np.where(label >= thresh_2, 0.5, 0)
-            return new_label
-        
-        return label
