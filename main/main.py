06ad8c133e5e3553888bc5075ded1cf8a1afb64a
hliu35 <hliu35@ucsc.edu>
2022-05-10 20:16:53 -0700

    changed to 16-dim embeddings

--- main/main.py
+++ main/main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import shutil
 import argparse
+import random
 
 sys.path.insert(0, '../model')
 sys.path.insert(0, '../utils')
@@ -31,7 +32,7 @@ from synth_labels import synthesize_onehot_labels as synth_onehot
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=50, help="number of epochs of training")
+parser.add_argument("--epochs", type=int, default=50, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=8, help="size of the batches") # changed from 16 to 8
 parser.add_argument("--lr", type=float, default=0.0005, help="adam: learning rate") # changed from 0.0002
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
@@ -39,9 +40,10 @@ parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of firs
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space") # original: 100, new: 16
 parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")  # changed from 64 to 128
-parser.add_argument('--n_classes', type=int, default=4, help='number of classes for dataset')
+#parser.add_argument('--n_classes', type=int, default=4, help='number of classes for dataset')
+parser.add_argument('--n_classes', type=int, default=16, help='number of classes for dataset')
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=50, help="interval betwen image samples")
+parser.add_argument("--sample_interval", type=int, default=200, help="interval betwen image samples")
 opt = parser.parse_args()
 
 
@@ -52,7 +54,10 @@ CUDA = True if torch.cuda.is_available() else False
 
 def list_full_paths(directory):
     full_list = [os.path.join(directory, file) for file in os.listdir(directory) if "png" in file]
-    return sorted(full_list)
+    random.shuffle(full_list)
+    #test_list = [x for x in full_list if "Day 15" in x] # test with only day 15 data
+    return full_list
+    #return test_list
 
 
 def run_gan(datapath, annotation_file, outpath="../tmp/"):
@@ -64,7 +69,8 @@ def run_gan(datapath, annotation_file, outpath="../tmp/"):
 
     # retrieve the list of image paths
     img_list = list_full_paths(datapath)
-    print(img_list[:10])
+    #print(img_list)
+    #exit()
 
     # Loss function
     adversarial_loss = torch.nn.BCELoss()
@@ -80,6 +86,7 @@ def run_gan(datapath, annotation_file, outpath="../tmp/"):
 
     # Configure data loader and compose transform functions
     TRANSFORMS = T.Compose([T.ToTensor(), T.Resize((opt.img_size, opt.img_size))])
+    #TRANSFORMS = T.Compose([T.Grayscale(num_output_channels=1), T.ToTensor(), T.Resize((opt.img_size, opt.img_size))])
     dataset = WoundImageDataset(img_list, \
         annotation_file, \
         transform = TRANSFORMS) # can also customize transform
@@ -96,7 +103,7 @@ def run_gan(datapath, annotation_file, outpath="../tmp/"):
     #  Training
     # ----------
 
-    for epoch in range(opt.n_epochs):
+    for epoch in range(opt.epochs):
         for i, (imgs, _) in enumerate(dataloader):
 
             # SKIP BATCH SIZE OF 1
@@ -116,7 +123,7 @@ def run_gan(datapath, annotation_file, outpath="../tmp/"):
             optimizer_G.zero_grad()
 
             # Sample noise as generator input
-            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+            z = Variable(Tensor(np.random.normal(0, 100, (imgs.shape[0], opt.latent_dim))))
 
             # Generate a batch of images
             gen_imgs = generator(z)
@@ -143,12 +150,12 @@ def run_gan(datapath, annotation_file, outpath="../tmp/"):
 
             print(
                 "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-                % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+                % (epoch, opt.epochs, i, len(dataloader), d_loss.item(), g_loss.item())
             )
 
             batches_done = epoch * len(dataloader) + i
             if batches_done % opt.sample_interval == 0:
-                save_image(gen_imgs.data[:25], os.path.join(outpath, "%d.png" % batches_done), nrow=5, normalize=True)
+                save_image(gen_imgs.data[:25], os.path.join(outpath, "%d.png" % batches_done), nrow=4, normalize=True)
 
 
 
@@ -199,14 +206,15 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
     #optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=opt.lr)            ########## optimizer changed from default
     #optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=opt.lr)
-
-
-    Tensor = torch.cuda.FloatTensor if CUDA else torch.FloatTensor
-
+    
 
     batches_done=0
-    for epoch in range(opt.n_epochs):
-        for i, (imgs, labels) in enumerate(dataloader):
+    for epoch in range(opt.epochs):
+        #for i, (imgs, labels) in enumerate(dataloader):
+        for i, (imgs, _, labels) in enumerate(dataloader):
+
+            #print(labels)
+            #exit()
 
             batch_size = opt.batch_size
 
@@ -231,9 +239,9 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
             #gen_labels = (torch.rand(batch_size, 1) * n_classes).type(torch.LongTensor)
             #gen_y = torch.zeros(batch_size, n_classes)
             #gen_y = Variable(gen_y.scatter_(1, gen_labels.view(batch_size, 1), 1).cuda())
-            
-            gen_y = synth_softmax(n_classes=opt.n_classes, batch_size=batch_size).to("cuda")
+            gen_y = synth_softmax(n_classes=opt.n_classes, batch_size=batch_size).to("cuda") #     SYNTH 16-dim embeddings?     ????????????????????????????????????????????????????????????????
             #gen_y = synth_onehot(n_classes=opt.n_classes, batch_size=batch_size, fixed=False).to("cuda")
+            
             #print(gen_y)
             #exit()
             
@@ -269,7 +277,13 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
             optimizer_D.step()
 
 
-            print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(dataloader),
+            # test with Gradient Clipping
+            _clip = 0.25
+            torch.nn.utils.clip_grad_norm(generator.parameters(), _clip)
+            torch.nn.utils.clip_grad_norm(discriminator.parameters(), _clip)
+
+            # print results
+            print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.epochs, i, len(dataloader),
                                                                 d_loss.data.cpu(), g_loss.data.cpu()))
 
             batches_done = epoch * len(dataloader) + i
@@ -286,7 +300,8 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
                 gen_imgs = generator(noise, y_fixed).view(-1, *img_shape)
                 save_image(gen_imgs.data, os.path.join(outpath, '%d-%d.png' % (epoch,batches_done)), nrow=n_classes, normalize=True) # nrow = number of img per row
 
-        torch.save(generator, os.path.join(outpath, "cgan_gen.pth"))
+        if epoch % 10 == 0:
+            torch.save(generator, os.path.join(outpath, "cgan_gen.pth"))
 
 
 
@@ -299,11 +314,12 @@ if __name__ == "__main__":
     
     # configure data and annotation path
     datapath = "../data_augmented/"
-    annotation_file = "../data_augmented/augmented_labels.csv"
+    #annotation_file = "../data_augmented/augmented_labels.csv"
+    annotation_file = "../data_augmented/augmented_data.csv"
 
     # train/test the models
-    run_gan(datapath, annotation_file)
-    #run_cgan(datapath, annotation_file)
+    #run_gan(datapath, annotation_file)
+    run_cgan(datapath, annotation_file)
 
     # custom test scripts
     
