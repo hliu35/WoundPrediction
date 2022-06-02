915a321f404791dad6366e7a48a6e07f8b3896a8
hliu35 <hliu35@ucsc.edu>
2022-06-02 16:52:58 -0700

    added temporal loss with correctly ordered pairs

--- main/train.py
+++ main/train.py
@@ -26,8 +26,11 @@ from itertools import combinations
 
 
 import gan, dcgan
-import classifier as CLS
-from dataloader import WoundImageDataset
+import temporal_encoder as TE
+import temporal_classifier as TC
+
+#from dataloader import WoundImageDataset
+from dataloader_ijk import WoundImagePairsDataset # new dataset with day i and j
 from synth_labels import synthesize_softmax_labels as synth_softmax
 from synth_labels import synthesize_onehot_labels as synth_onehot
 
@@ -63,8 +66,19 @@ def list_full_paths(directory):
     return full_list
     #return test_list
 
-def list_full_paths_combs(directory):
+def list_full_paths_combs(directory, mode="train"):
+
     full_list = [os.path.join(directory, file) for file in os.listdir(directory) if "png" in file]
+    test_imgs = [x for x in full_list if "Y8-4-L" in x or "A8-1-R" in x]
+    val_imgs = [x for x in full_list if "Y8-4-R" in x or "A8-1-L" in x]
+    train_imgs = set(full_list).difference(set(test_imgs))
+    train_imgs = train_imgs.difference(set(val_imgs))
+    train_imgs = list(train_imgs)
+
+    if mode=="train": full_list = train_imgs
+    elif mode=="val": full_list = val_imgs
+    else: full_list = test_imgs
+
     Ids = []
     for i in range(16):
         temp = full_list[i].split("/")[-1]
@@ -89,8 +103,8 @@ def list_full_paths_combs(directory):
     return combs_list
 
 
-def run_gan(datapath, annotation_file, outpath="../tmp/"):
-
+def train_gan(datapath, annotation_file, outpath="../tmp/"):
+    ''' This is the script to train Vanilla GAN '''
     # create output folder
     if os.path.exists(outpath):
         shutil.rmtree(outpath)
@@ -189,7 +203,8 @@ def run_gan(datapath, annotation_file, outpath="../tmp/"):
 
 
 
-def run_cgan(datapath, annotation_file, outpath="../tmp/"):
+def train_cgan(datapath, annotation_file, outpath="../tmp/"):
+    ''' This is the script to train Conditional GAN a.k.a. DCGAN '''
     # https://www.reddit.com/r/MachineLearning/comments/5asl74/discussion_discriminator_converging_to_0_loss_in/
 
     C = opt.n_classes
@@ -201,18 +216,15 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
     os.mkdir(outpath)
 
     # retrieve the list of image paths
-    img_list = list_full_paths(datapath)
-
-    test_imgs = [x for x in img_list if "Y8-4-L" in x or "A8-1-R" in x]
-    val_imgs = [x for x in img_list if "Y8-4-R" in x or "A8-1-L" in x]
-    train_imgs = set(img_list).difference(set(test_imgs))
-    train_imgs = train_imgs.difference(set(val_imgs))
-    train_imgs = list(train_imgs)
+    #img_list = list_full_paths(datapath)
+    train_imgs = list_full_paths_combs(datapath, "train")
+    #val_imgs = list_full_paths_combs(datapath, "val")
+    #test_imgs = list_full_paths_combs(datapath, "test")
 
 
-    # Loss function
-    adversarial_loss = torch.nn.BCELoss()  ######## TEST with MSE
-    #adversarial_loss = torch.nn.MSELoss()
+    # Loss functions for part 1, 2, and 3
+    adversarial_loss = torch.nn.BCELoss()
+    #adversarial_loss = torch.nn.MSELoss() # an alternative loss function
     
     if C == 16:
         # if it's 16, they are not binary values and should be treated as random numbers
@@ -221,38 +233,49 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
     else:
         # if it's 4 (stages), then it must be binary cross entropy
         embedding_loss = torch.nn.BCELoss()
-        
+    
+    temporal_loss = torch.nn.BCELoss()
 
 
     # Initialize Generator and discriminator
     generator = dcgan.Generator(IMG_SHAPE, opt.latent_dim, C)
     discriminator = dcgan.Discriminator(IMG_SHAPE, C)
-    
-    classifier = CLS.loadClassifier("../model/best_classifier.tar")
-    for p in classifier.parameters():
+
+
+    # Initialize Temporal Encoder
+    temporal_encoder = TE.loadClassifier("../model/best_classifier.tar")
+    for p in temporal_encoder.parameters():
         p.require_grads = False
-    classifier.eval()
+    temporal_encoder.eval()
+
+    # Initialize Temporal Discriminator
+    tc_latent_dim = 32
+    temporal_classifier = TC.TemporalClassifier(IMG_SHAPE, tc_latent_dim)
+
 
     # Initialize weights
     #generator.apply(weights_init_normal)
     #discriminator.apply(weights_init_normal)
 
+    
     if CUDA:
         generator.cuda()
         discriminator.cuda()
+        temporal_encoder.cuda()
+        temporal_classifier.cuda()
+
         adversarial_loss.cuda()
+        embedding_loss.cuda()
+        temporal_loss.cuda()
 
-    # Configure data loader and compose transform functions
+
+    # Configure data loaders and compose transform functions
     TRANSFORMS = T.Compose([T.ToTensor(), T.Resize((opt.img_size, opt.img_size))])
-    #dataset = WoundImageDataset(img_list, \
-    #    annotation_file,\
-    #    transform = TRANSFORMS) # can also customize transform
 
-    #dataloader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
 
-    train_dataset = WoundImageDataset(train_imgs, annotation_file, transform = TRANSFORMS)
-    #val_dataset = WoundImageDataset(val_imgs, annotation_file, transform = TRANSFORMS)
-    #test_dataset = WoundImageDataset(test_imgs, annotation_file, transform = TRANSFORMS)
+    train_dataset = WoundImagePairsDataset(train_imgs, annotation_file, transform = TRANSFORMS)
+    #val_dataset = WoundImagePairsDataset(val_imgs, annotation_file, transform = TRANSFORMS)
+    #test_dataset = WoundImagePairsDataset(test_imgs, annotation_file, transform = TRANSFORMS)
 
     train_dataloader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
     #val_dataloader = DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=False)
@@ -273,13 +296,13 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
 
     batches_done=0
     for epoch in range(opt.epochs):
-        #for i, (imgs, Y4, Y16) in enumerate(dataloader):
-        for i, (imgs, Y4, Y16) in enumerate(train_dataloader):
+
+        for i, (imgs_i, imgs_j, imgs_k, Y4, Y16) in enumerate(train_dataloader):
             # training parameters
             B = opt.batch_size
 
             # SKIP BATCH SIZE OF 1
-            if imgs.shape[0] < B: continue
+            if imgs_k.shape[0] < B: continue
 
 
             # Sample labels as generator input
@@ -303,7 +326,9 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
             fake = Variable(torch.zeros(B).cuda(), requires_grad=False)
 
             # Configure input
-            real_imgs = Variable(imgs.type(torch.FloatTensor).cuda())
+            imgs_k = Variable(imgs_k.type(torch.FloatTensor).cuda())
+            imgs_i = Variable(imgs_i.type(torch.FloatTensor).cuda())
+            imgs_j = Variable(imgs_j.type(torch.FloatTensor).cuda())
             #real_y = torch.zeros(batch_size, n_classes)
             #real_y = Variable(real_y.scatter_(1, labels.view(batch_size, n_classes), 1).cuda())
 
@@ -332,26 +357,49 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
 
             optimizer_D.zero_grad()
 
+            # REALISM LOSS (Loss R)
+
             # Loss for real images
-            d_real_loss = adversarial_loss(discriminator(real_imgs, real_y).squeeze(), valid)
+            d_real_loss = adversarial_loss(discriminator(imgs_k, real_y).squeeze(), valid)
             # Loss for fake images
             d_fake_loss = adversarial_loss(discriminator(gen_imgs.detach(),gen_y).squeeze(), fake)
             
-            # Loss for embeddings
+            
+            
+            # EMBEDDING LOSS (Loss yhat|z)
+
             cls_input = gen_imgs.detach()
             cls_input = transform_densenet(cls_input)
 
             target = gen_y
 
-            label_pred, fake_embeddings = classifier(cls_input)         # NOT SURE IF DETACHING AGAIN IS CORRECT FOR LOSS COMPUTATION
+            label_pred, fake_embeddings = temporal_encoder(cls_input)         # NOT SURE IF DETACHING AGAIN IS CORRECT FOR LOSS COMPUTATION
             prediction = F.softmax(label_pred) if C == 4 else fake_embeddings # label_pred has not been softmaxed yet
 
             emb_loss = embedding_loss(target, prediction)
+
             
+            # TEMPORAL COHERENCE LOSS (Loss Xpred_k | Xi, Xj)
+            Yirk = torch.ones((B,1)) # i and real k
+            Yjrk = torch.ones((B,1)) # j and real k
+            Yifk = torch.zeros((B,1)) # i and fake k
+            Yjfk = torch.zeros((B,1)) # j and fake k
+
+            gen_k = gen_imgs.detach()
+
+            temporal_target = torch.cat((Yirk, Yjrk, Yifk, Yjfk), dim=0).cuda()
+            temporal_K = torch.cat((imgs_k, imgs_k, gen_k, gen_k), dim=0)
+            temporal_IJ = torch.cat((imgs_i, imgs_j, imgs_i, imgs_j), dim=0)
+
+            temporal_pred = temporal_classifier(temporal_IJ, temporal_K)
+
+            t_loss = temporal_loss(temporal_pred, temporal_target)
+
             
-            # Total discriminator loss
+            # TOTAL DISCRIMINATOR LOSS (L)
+
             #d_loss = (d_real_loss + d_fake_loss)
-            d_loss = (d_real_loss + d_fake_loss + emb_loss)
+            d_loss = (d_real_loss + d_fake_loss + emb_loss + t_loss)
 
             
             # conditional update D
@@ -374,8 +422,8 @@ def run_cgan(datapath, annotation_file, outpath="../tmp/"):
                 # y_disp was defined at the very beginning of training
                 #print(y_disp)
                 gen_imgs = generator(noise, y_disp).view(-1, *IMG_SHAPE)
-                #save_image(gen_imgs.data, os.path.join(outpath, '%d-%d.png' % (epoch,batches_done)), nrow=C//4, normalize=True) # nrow = number of img per row, original C, current C//4
-                save_image(imgs.data, os.path.join(outpath, '%d-%d.png' % (epoch,batches_done)), nrow=C//2, normalize=True) # real data
+                save_image(gen_imgs.data, os.path.join(outpath, '%d-%d.png' % (epoch,batches_done)), nrow=C//4, normalize=True) # nrow = number of img per row, original C, current C//4
+                #save_image(imgs_k.data, os.path.join(outpath, '%d-%d.png' % (epoch,batches_done)), nrow=C//2, normalize=True) # real data
 
         if epoch % 10 == 0:
             torch.save(generator, os.path.join(outpath, "cgan_gen.pth"))
@@ -397,5 +445,5 @@ if __name__ == "__main__":
     annotation_file = "../data_augmented/augmented_data.csv"
 
     # train/test the models
-    #run_gan(datapath, annotation_file)
-    run_cgan(datapath, annotation_file)
+    #train_gan(datapath, annotation_file)
+    train_cgan(datapath, annotation_file)
