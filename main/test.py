613f8f3a29bc35a1e80652d231e87dc49171ff02
hliu35 <hliu35@ucsc.edu>
2022-06-06 13:56:46 -0700

    corrected image k choices

--- main/test.py
+++ main/test.py
@@ -16,8 +16,8 @@ import torch
 
 
 
-import gan, dcgan
-from train import list_full_paths
+import dcgan
+from train_cgan import list_full_paths, unshift
 
 #from dataloader import WoundImageDataset
 from dataloader import WoundImagePairsDataset # new dataset with day i and j
@@ -38,8 +38,18 @@ def test_cgan(datapath, annotation_file, outpath="../test_results/"):
     os.mkdir(outpath)
 
     # retrieve the list of image paths
-    test_imgs = list_full_paths(datapath, "test")[:14] # day 2-15 of mice a8-1-r
+    img_pairs = list_full_paths(datapath, mode="test")
+    test_imgs = []
+
+    for i in range(2, 16):
+        text = "/Day %d_A8-1-R"%i
+        for p in img_pairs:
+            if text in p[2]:
+                test_imgs.append(p)
+                break
     
+    for x in test_imgs:
+        print(x[2])
 
     # Initialize Generator and discriminator
     generator = torch.load("../tmp/cgan_gen.pth")
@@ -50,25 +60,31 @@ def test_cgan(datapath, annotation_file, outpath="../test_results/"):
 
 
     # Configure data loaders and compose transform functions
-    TRANSFORMS = T.Compose([T.ToTensor(), T.Resize((128, 128))])
+    TRANSFORMS = T.Compose([T.ToTensor(), T.Resize((128, 128)), T.Normalize(0.5, 0.5)])
 
     test_dataset = WoundImagePairsDataset(test_imgs, annotation_file, transform = TRANSFORMS)
     test_dataloader = DataLoader(test_dataset, batch_size=B, shuffle=False)
 
-    for i, (_, _, imgs_k, _, Y16) in enumerate(test_dataloader):
+    for _, (_, _, imgs_k, _, Y16) in enumerate(test_dataloader):
         y_disp = Y16.cuda() # labels for display
         
         # sample noise for generating fakes
-        #noise = Variable(torch.randn((B, 100)).cuda())
-        #imgs_k = Variable(imgs_k.type(torch.FloatTensor).cuda())
         noise = torch.randn((B, 100)).cuda()
         imgs_k = imgs_k.type(torch.FloatTensor).cuda()
 
 
         # y_disp was defined at the very beginning of training
         gen_imgs = generator(noise, y_disp).view(-1, *IMG_SHAPE)
-        save_image(gen_imgs.data, os.path.join(outpath, 'generated_a8-1-r-day2to15.png'), nrow=7, normalize=True) 
-        save_image(imgs_k.data, os.path.join(outpath, 'true_a8-1-r-day2to15.png'), nrow=7, normalize=True) # real data
+        
+        # shift the range back to [-1, 1]
+        gen_imgs = unshift(gen_imgs)
+        imgs_k = unshift(imgs_k)
+
+        gen_imgs = gen_imgs * 0.5 + 0.5
+        imgs_k = imgs_k * 0.5 + 0.5
+
+        save_image(gen_imgs.data, os.path.join(outpath, 'generated_a8-1-r-day2to15.png'), nrow=7, normalize=False) 
+        save_image(imgs_k.data, os.path.join(outpath, 'true_a8-1-r-day2to15.png'), nrow=7, normalize=False) # real data
         break
 
 if __name__ == "__main__":
