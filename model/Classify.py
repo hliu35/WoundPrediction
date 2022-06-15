087f8cd6e8be329247ce3fa9e7a2952b6632731e
Saif <skausar@ucsc.edu>
2022-06-14 17:49:52 -0700

    updated TemporalGeneratorTest and Classify for testing

--- model/Classify.py
+++ model/Classify.py
@@ -1,5 +1,5 @@
-from ctypes.wintypes import RGB
-from temporal_encoder import loadClassifier
+from msilib.schema import Class
+import temporal_encoder
 import os
 from PIL import Image
 import torchvision.transforms as transforms
@@ -10,19 +10,29 @@ import csv
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-Classifier = loadClassifier()
+Classifier = temporal_encoder.Classifier_Encoder()
+Classifier = Classifier.to(device)
+path = "checkpoints/normalized_classifier.tar"
+Classifier.load_state_dict(torch.load(path))
 Classifier.eval()
 
 transform = transforms.Compose([
             transforms.Resize(224),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+            transforms.Normalize([0.56014212, 0.40342121, 0.32133712], [0.20345279, 0.14542403, 0.12238597])])
 
 labels = []
+wait = 0 + 55 + 55 + 55 + 55 + 55 + 55 + 55 + 55 + 55 + 55
 count = 0
+imgPaths = []
 for imgPath in os.listdir('images'):
-    if(count == 55):
+    if(count < wait):
+        count += 1
+        continue
+    if(len(imgPaths) == 55):
         break
+    imgPaths.append(imgPath)
+for imgPath in imgPaths:
     with open('images/' + imgPath, 'rb') as f:
         img = Image.open('images/' + imgPath)
         img.convert('RGB')
@@ -30,12 +40,18 @@ for imgPath in os.listdir('images'):
     img_tensor = torch.stack([img_tensor])
     labelpred, _ = Classifier(img_tensor.to(device))
     labels.append(F.softmax(labelpred))
-    count += 1
 
-with open('labels.csv', mode = 'w') as f:
+with open('dist.csv', mode = 'w', newline="") as f:
     writer = csv.writer(f)
     for lab in labels:
-        temp = np.array2string(lab.cpu().detach().numpy()[0], separator=', ')
+        #lab = torch.argmax(lab) + 1
+        temp = np.array2string(lab.cpu().detach().numpy(), separator=', ')
+        writer.writerow([temp])
+with open('labels.csv', mode = 'w', newline="") as f:
+    writer = csv.writer(f)
+    for lab in labels:
+        lab = torch.argmax(lab) + 1
+        temp = np.array2string(lab.cpu().detach().numpy(), separator=', ')
         writer.writerow([temp])
 
 print(len(labels))
--- model/TemporalGeneratorTest.py
+++ model/TemporalGeneratorTest.py
@@ -7,7 +7,7 @@ def LinearInter(EmbOne, EmbTwo, distEmb, distFin):
     inter = []
     for i, j in zip(EmbOne, EmbTwo):
         diff = j - i
-        diff = diff/distEmb
+        diff = diff/distEmb * 0.5
         inter.append(j + diff * distFin)
     return inter
 
@@ -31,7 +31,7 @@ def GenPairs(embedingList, MouseID):
 
 #Create a list of the embeddings
 embeddings = []
-with open('data/embeddings.csv', 'r') as file:
+with open('data/normalized_embeddings.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
       embeddings.append(row)
@@ -53,22 +53,22 @@ for embed in embeddings:
   temp.insert(0,day)
   values.append(temp)
 
-MouseID = 'Y8-4-R'
+MouseID = 'Y8-4-L'
 temp = GenPairs(values, MouseID)
 
 
-GenModel = torch.load("models\cgan_gen_all_loss_0603.pth")
+GenModel = torch.load("models\cgan_gen_0610.pth")
 GenModel.eval()
 
 
 for pair in temp:
-    dist = 1
-    if (pair[1][0] + dist > 15):
-        continue
-    emb = LinearInter(pair[0][1:], pair[1][1:], pair[1][0] - pair[0][0], dist)
-    emb = torch.stack([torch.Tensor(emb).cuda()])
-    noise = Variable(torch.randn((1, 100)).cuda())
-    img_shape = (3, 128, 128)
-    img = GenModel(noise, emb).view(-1, *img_shape)
-    save_image(img.data, "images/" + MouseID + "-" + chr(pair[0][0] + 97) + "-" + chr(pair[1][0]+97) +  ".png")
+    for dist in range(1, 15):
+        if (pair[1][0] + dist > 15):
+            continue
+        emb = LinearInter(pair[0][1:], pair[1][1:], pair[1][0] - pair[0][0], dist)
+        emb = torch.stack([torch.Tensor(emb).cuda()])
+        noise = Variable(torch.randn((1, 100)).cuda())
+        img_shape = (3, 128, 128)
+        img = GenModel(noise, emb).view(-1, *img_shape)
+        save_image(img.data, "images/" + MouseID + "-" + chr(pair[0][0] + 97) + "-" + chr(pair[1][0]+97) +  "-" + chr(pair[1][0]+dist+97) + ".png")
 
